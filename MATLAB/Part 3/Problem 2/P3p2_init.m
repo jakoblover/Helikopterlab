4b48dce5cc96754b88605fd4668e036854f3f29a
Jakob Lover <jakob.lover@ebnett.no>
2017-10-20 19:12:00 +0200

    Part 3

--- MATLAB/Part 3/Problem 2/P3p2_init.m
+++ MATLAB/Part 3/Problem 2/P3p2_init.m
@@ -38,47 +38,16 @@ j_lambda = m_c*l_c^2 + 2*m_p*(l_h^2 + l_p^2);
 k_1 = l_1 /j_p;
 k_2 = l_3 / j_e;
 k_3 = (-l_4*l_2) / (j_lambda * l_3);
-%w_n = 0.35;
-
-%k_pp = k_1/w_n^2;
-%k_pd = (2/w_n)*k_pp;
-
-k_pp = 8;
-k_pd = 10;
-
-k_pt = 10;
 
 
-A = [0 1 0 0 0; 0 0 0 0 0; 0 0 0 0 0; 1 0 0 0 0; 0 0 1 0 0];
-B = [0 0 ; 0 k_1 ; k_2 0; 0 0; 0 0];
+%%%%%%%%%%%%%%%%%%% Control system design
+A = [0 1 0; 0 0 0; 0 0 0];
+B = [0 0 ; 0 k_1 ; k_2 0];
 C = [1 0 0; 0 0 1];
-Q = diag([100 1000 100 100000 10000]); %pitch, pitch rate, elevation rate, pitch integral, elevation integral
+Q = diag([100 1000 100]); %pitch, pitch rate, elevation rate, pitch integral, elevation integral
 R = diag([10 1000]);
+
 K = lqr(A,B,Q,R);
+P = inv(C*(inv(B*K - A)) * B);
+%P = [0 31.6228; 1 0];
 
-%C_ctrl = [B A*B (A*A)*B];
-%D = zeros(2,2);
-
-%f = K(1,3);
-%g = K(2,1);
-%h = K(2,2); 
-
-%sys = ss(A,B,C,D);
-%controll = rank(ctrb(sys));
-%controll = rank(C);
-% P utrekna fra formel i slides
-%P = inv(C*(inv(B*K - A)) * B);
-P = [0 31.6228; 1 0];
-
-A_obs = [0 1 0 0 0 0; 0 0 0 0 0 0; 0 0 0 1 0 0; 0 0 0 0 0 0; 0 0 0 0 0 1; k_3 0 0 0 0 0];
-C_obs = [1 0 0 0 0 0; 0 0 1 0 0 0; 0 0 0 0 1 0];
-
-CA1 = C_obs * A_obs;
-CA2 = C_obs * A_obs * A_obs;
-CA3 = C_obs * A_obs * A_obs * A_obs;
-CA4 = C_obs * A_obs * A_obs * A_obs * A_obs;
-CA5 = C_obs * A_obs * A_obs * A_obs * A_obs * A_obs;v 
-
-O = [C_obs;CA1;CA2;CA3;CA4;CA5];
-
-controll = rank(O);
